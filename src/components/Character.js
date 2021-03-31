612d06085914422632fc2f96d20248369f1ca025
Petar Todorov <petar.ptodorov@gmail.com>
2021-03-31 23:47:05 +0300

    feat: add monster eyes moving & blinking

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,10 +1,13 @@
-import { Container, Sprite } from "pixi.js";
+import { TimelineLite } from 'gsap/gsap-core';
+import { Container, Sprite } from 'pixi.js';
 import Assets from '../core/AssetManager';
+import { random } from '../core/utils';
 
 export default class Character extends Container {
   constructor(openedEyes = false) {
     super();
     this.init(openedEyes);
+    this.interactive = true;
   }
 
   async init(openedEyes) {
@@ -42,20 +45,62 @@ export default class Character extends Container {
     const monsterEyeLidTop = new Sprite.from('monster-eye-lid-top');
     const monsterEyeLidBottom = new Sprite.from('monster-eye-lid-bottom');
 
-    monsterEye.x = 105;
-    monsterEye.y = 100;
+    this.monsterEye = monsterEye;
+    this.monsterEye.anchor.set(0.5);
+    this.monsterEyeLidTop = monsterEyeLidTop;
+    this.monsterEyeLidBottom = monsterEyeLidBottom;
 
-    monsterEyeLidTop.x = 88;
-    monsterEyeLidTop.y = 84;
+    this.monsterEye.x = 132;
+    this.monsterEye.y = 125;
 
-    monsterEyeLidBottom.x = 88;
-    monsterEyeLidBottom.y = 134;
+    this.monsterEyeLidTop.x = 88;
+    this.monsterEyeLidTop.y = 84;
+
+    this.monsterEyeLidBottom.x = 88;
+    this.monsterEyeLidBottom.y = 134;
 
     this.addChild(monsterBody);
-    this.addChild(monsterEye);
+    this.addChild(this.monsterEye);
     if (!openedEyes) {
-      this.addChild(monsterEyeLidTop);
-      this.addChild(monsterEyeLidBottom);
+      this.addChild(this.monsterEyeLidTop);
+      this.addChild(this.monsterEyeLidBottom);
+      this._startBlinking();
     }
+
+    this.on('pointermove', this._moveEyesToCursor);
+
+  }
+
+  _startBlinking() {
+    const randomRepeatTime = random(7, 15);
+
+    const timeline = new TimelineLite();
+
+    timeline.to(this.monsterEyeLidTop, { pixi: { scaleX: 1.065, scaleY: 1.25, positionX: this.monsterEyeLidTop.x - 2 } }, 'close');
+    timeline.to(this.monsterEyeLidBottom, { pixi: { scaleX: 1.065, scaleY: 1.25, positionX: this.monsterEyeLidBottom.x - 2, positionY: this.monsterEyeLidBottom.y - 10 } }, 'close');
+
+    timeline.to(this.monsterEyeLidTop, { pixi: { scaleX: 1, scaleY: 1, positionX: this.monsterEyeLidTop.x }, }, 'open');
+    timeline.to(this.monsterEyeLidBottom, { pixi: { scaleX: 1, scaleY: 1, positionX: this.monsterEyeLidBottom.x, positionY: this.monsterEyeLidBottom.y } }, 'open');
+
+    timeline.repeat(-1).repeatDelay(randomRepeatTime);
+  }
+
+  _moveEyesToCursor(e) {
+    const coordinates = e.data.global;
+
+    const cursorX = coordinates.x;
+    const cursorY = coordinates.y;
+
+    const monsterWorldPosition = this.monsterEye.worldTransform;
+
+    const getXDistance = cursorX - monsterWorldPosition.tx;
+    const getYDistance = cursorY - monsterWorldPosition.ty;
+
+    const radianDegrees = Math.atan2(getXDistance, getYDistance);
+    const rotationDegrees = (radianDegrees * (180 / Math.PI) * -1) + 180;
+
+    this.monsterEye.anchor.set(0.55);
+    this.monsterEye.angle = rotationDegrees + 90;
+
   }
 }
